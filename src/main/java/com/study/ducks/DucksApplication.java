27bbcbfd380597499ce3190faddbb0c02488a317
jjim1004 <jjlim1004@naver.com>
2025-02-27 23:22:42 +0900

    feat: ducks 기본 프로젝트 생성
    https://github.com/lifeandsong/SpringBootGradleTemplate
    참고해서 작성함

---
+++ src/main/java/com/study/ducks/DucksApplication.java
@@ -0,0 +1,13 @@
+package com.study.ducks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucksApplication.class, args);
+	}
+
+}
